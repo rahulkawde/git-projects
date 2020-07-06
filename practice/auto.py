0982e91d9bae5a1fecc064d6980fbfda13d2b114
rahul <kawderahul999@gmail.com>
2020-07-06 14:57:13 +0530

    clean

--- practice/auto.py
+++ practice/auto.py
@@ -3,3 +3,8 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get('http://youtube.com')
+searchox = driver.find_element_by_xpath('//*[@id="search"]')
+searchox.send_keys('rahul kawde')
+
+click_btn = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]/yt-icon') 
+click_btn.click()
